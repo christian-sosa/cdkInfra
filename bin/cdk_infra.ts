50e50030f8b73da076d9086f6597a2c89dd6709b
christian sosa <csosa@arkho.tech>
2022-02-06 20:57:01 -0300

    add stacks

--- bin/cdk_infra.ts
+++ bin/cdk_infra.ts
@@ -5,4 +5,22 @@ import { RDSStack } from '../lib/rds-stack';
 import { PROPS } from './props/props';
 
 const app = new cdk.App();
-new RDSStack(app,'FalpRDSStackPRD',PROPS.rds1)
+new RDSStack(app,'RDS1',PROPS.rds1)
+
+new RDSStack(app,'RDS2',PROPS.rds2)
+
+new RDSStack(app,'RDS3',PROPS.rds3)
+
+new RDSStack(app,'RDS4',PROPS.rds4)
+
+new RDSStack(app,'RDS5',PROPS.rds5)
+
+new RDSStack(app,'RDS6',PROPS.rds6)
+
+new RDSStack(app,'RDS7',PROPS.rds7)
+
+new RDSStack(app,'RDS8',PROPS.rds8)
+
+new RDSStack(app,'RDS9',PROPS.rds9)
+
+new RDSStack(app,'RDS10',PROPS.rds10)
