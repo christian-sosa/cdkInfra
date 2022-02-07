a1967d4eb1a8d9b7667c1b9e7a1e1218c9964021
christian sosa <csosa@arkho.tech>
2022-02-06 21:29:49 -0300

    se soluciono dotenv error

--- bin/props/props.ts
+++ bin/props/props.ts
@@ -1,141 +1,134 @@
 require('dotenv').config()
-import { ProcessEnv, ProcessEnv2 } from "../../interfaces/env"
+import { IVpcProps } from "../../interfaces/vpc-props";
 
-const AZPS: ProcessEnv2 ={
-    az1: process.env.aZs11,
-    az2: process.env.aZs12,
-    ps1:  process.env.privateSubnets11,
-    ps2: process.env.privateSubnets12
 
-}
-
-const vpcStackPRD = {
-    env: { account: process.env.account, region: process.env.region },
+const vpcStackPRD: IVpcProps = {
+    env: { account: 'XXXX', region: 'us-east-2' },
     aZs: [
-        process.env.aZsVPC1,
-        process.env.aZsVPC2
+        'us-east-2a',
+        'us-east-2b'
     ],
-    vpcCidr: process.env.vpcCidr,
-    stage: process.env.stage
+    vpcCidr: '0.0.0.0/0',
+    stage: 'PROD'
 }
 
 const rds1 = {
-    env: { account: process.env.account, region: process.env.region },
-    stage: process.env.stage, 
-    vpcId: process.env.vpcId,
-    securityGroupId: process.env.securityGroupId,
-    aZs: [AZPS.az1,AZPS.az2] ,
+    env: { account: 'XXXX', region: 'us-east-2' },
+    stage:'PROD',  
+    vpcId:'vpc-XXX',
+    securityGroupId:'sg-XXX',
+    aZs:['us-east2a','us-east2b'],
     privateSubnets:[
-        AZPS.ps1,
-        AZPS.ps2
+        'subnet-XXXX',
+        'subnet-XXXX'
     ]
 }
 
 const rds2 = {
-    env: { account: process.env.account, region: process.env.region },
-    stage: process.env.stage, 
-    vpcId: process.env.vpcId2,
-    securityGroupId: process.env.securityGroupId2,
-    aZs: [process.env.aZs21,process.env.aZs22],
+    env: { account: 'XXXX', region: 'us-east-2' },
+    stage:'PROD',  
+    vpcId:'vpc-XXX',
+    securityGroupId:'sg-XXX',
+    aZs:['us-east2a','us-east2b'],
     privateSubnets:[
-        process.env.privateSubnets21,
-        process.env.privateSubnets22
+        'subnet-XXXX',
+        'subnet-XXXX'
     ]
 }
 
 const rds3 = {
-    env: { account: process.env.account, region: process.env.region },
-    stage: process.env.stage, 
-    vpcId: process.env.vpcId3,
-    securityGroupId: process.env.securityGroupId3,
-    aZs: [process.env.aZs31,process.env.aZs32],
+    env: { account: 'XXXX', region: 'us-east-2' },
+    stage:'PROD',  
+    vpcId:'vpc-XXX',
+    securityGroupId:'sg-XXX',
+    aZs:['us-east2a','us-east2b'],
     privateSubnets:[
-        process.env.privateSubnets31,
-        process.env.privateSubnets32
+        'subnet-XXXX',
+        'subnet-XXXX'
     ]
 }
 
 const rds4 = {
-    env: { account: process.env.account, region: process.env.region },
-    stage: process.env.stage, 
-    vpcId: process.env.vpcId4,
-    securityGroupId: process.env.securityGroupId4,
-    aZs: [process.env.aZs41,process.env.aZs42],
+    env: { account: 'XXXX', region: 'us-east-2' },
+    stage:'PROD',  
+    vpcId:'vpc-XXX',
+    securityGroupId:'sg-XXX',
+    aZs:['us-east2a','us-east2b'],
     privateSubnets:[
-        process.env.privateSubnets41,
-        process.env.privateSubnets42
+        'subnet-XXXX',
+        'subnet-XXXX'
     ]
 }
 
 const rds5 = {
-    env: { account: process.env.account, region: process.env.region },
-    stage: process.env.stage, 
-    vpcId: process.env.vpcId5,
-    securityGroupId: process.env.securityGroupId5,
-    aZs: [process.env.aZs51,process.env.aZs52],
+    env: { account: 'XXXX', region: 'us-east-2' },
+    stage:'PROD',  
+    vpcId:'vpc-XXX',
+    securityGroupId:'sg-XXX',
+    aZs:['us-east2a','us-east2b'],
     privateSubnets:[
-        process.env.privateSubnets51,
-        process.env.privateSubnets52
+        'subnet-XXXX',
+        'subnet-XXXX'
     ]
 }
 
 const rds6 = {
-    env: { account: process.env.account, region: process.env.region },
-    stage: process.env.stage, 
-    vpcId: process.env.vpcId6,
-    securityGroupId: process.env.securityGroupId6,
-    aZs: [process.env.aZs61,process.env.aZs62],
+    env: { account: 'XXXX', region: 'us-east-2' },
+    stage:'PROD',  
+    vpcId:'vpc-XXX',
+    securityGroupId:'sg-XXX',
+    aZs:['us-east2a','us-east2b'],
     privateSubnets:[
-        process.env.privateSubnets61,
-        process.env.privateSubnets62
+        'subnet-XXXX',
+        'subnet-XXXX'
     ]
 }
 
 const rds7 = {
-    env: { account: process.env.account, region: process.env.region },
-    stage: process.env.stage, 
-    vpcId: process.env.vpcId7,
-    securityGroupId: process.env.securityGroupId7,
-    aZs: [process.env.aZs71,process.env.aZs72],
+    env: { account: 'XXXX', region: 'us-east-2' },
+    stage:'PROD',  
+    vpcId:'vpc-XXX',
+    securityGroupId:'sg-XXX',
+    aZs:['us-east2a','us-east2b'],
     privateSubnets:[
-        process.env.privateSubnets71,
-        process.env.privateSubnets72
+        'subnet-XXXX',
+        'subnet-XXXX'
     ]
 }
 
 const rds8 = {
-    env: { account: process.env.account, region: process.env.region },
-    stage: process.env.stage, 
-    vpcId: process.env.vpcId8,
-    securityGroupId: process.env.securityGroupId8,
-    aZs: [process.env.aZs81,process.env.aZs82],
+    env: { account: 'XXXX', region: 'us-east-2' },
+    stage:'PROD',  
+    vpcId:'vpc-XXX',
+    securityGroupId:'sg-XXX',
+    aZs:['us-east2a','us-east2b'],
     privateSubnets:[
-        process.env.privateSubnets81,
-        process.env.privateSubnets82
+        'subnet-XXXX',
+        'subnet-XXXX'
     ]
 }
 
 const rds9 = {
-    env: { account: process.env.account, region: process.env.region },
-    stage: process.env.stage, 
-    vpcId: process.env.vpcId9,
-    securityGroupId: process.env.securityGroupId9,
-    aZs: [process.env.aZs91,process.env.aZs92],
+    env: { account: 'XXXX', region: 'us-east-2' },
+    stage:'PROD',  
+    vpcId:'vpc-XXX',
+    securityGroupId:'sg-XXX',
+    aZs:['us-east2a','us-east2b'],
     privateSubnets:[
-        process.env.privateSubnets91,
-        process.env.privateSubnets92
+        'subnet-XXXX',
+        'subnet-XXXX'
     ]
 }
 
 const rds10 = {
-    env: { account: process.env.account, region: process.env.region },
-    stage: process.env.stage, 
-    vpcId: process.env.vpcId10,
-    securityGroupId: process.env.securityGroupId10,
-    aZs: [process.env.aZs101,process.env.aZs102],
+    env: { account: 'XXXX', region: 'us-east-2' },
+    stage:'PROD',  
+    vpcId:'vpc-XXX',
+    securityGroupId:'sg-XXX',
+    aZs:['us-east2a','us-east2b'],
     privateSubnets:[
-        process.env.privateSubnets101,
-        process.env.privateSubnets102
+        'subnet-XXXX',
+        'subnet-XXXX'
     ]
 }
 
